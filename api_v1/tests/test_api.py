754fa4996d6aba42a7af845f4aee95bf0a5e5c5d
Alex Kiura <alex.kiura@andela.com>
2017-07-20 12:40:20 +0100

    [Chore] Test student API endpoints

--- api_v1/tests/test_api.py
+++ api_v1/tests/test_api.py
@@ -2,6 +2,7 @@
 
 from rest_framework.test import APITestCase
 from rest_framework import status
+import datetime
 
 
 class SchoolAPITest(APITestCase):
@@ -13,4 +14,25 @@ class SchoolAPITest(APITestCase):
              'category': 'dist', 'school_type': 'day',
              'gender': 'mixed'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class StudentAPITest(APITestCase):
+    def setUp(self):
+        response = self.client.post(
+            '/api/v1/schools/',
+            {'name': 'BrookHouse', 'email': 'brookhouse@gmail.com',
+             'category': 'dist', 'school_type': 'day',
+             'gender': 'mixed'})
+        self.school = response.data
+        
+    def test_student_creation(self):
+        """Test creating a student."""
+        response = self.client.post(
+            '/api/v1/students/',
+            {'name': 'Amaka', 'date_of_birth': '1995-07-23',
+             'school': str(self.school['id']), 'class_level': 4, 'gender': 'female'
+            })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
             
+
+        

ea754242bc18448b99ac81ec8d569710adfb409b
Alex Kiura <alex.kiura@andela.com>
2017-07-20 12:57:15 +0100

    [Chore] Refactor API docstrings

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,4 +1,4 @@
-"""API implementation using viewsets."""
+"""API views using viewsets."""
 
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
@@ -8,12 +8,32 @@ from .models import School, Student
 
 
 class SchoolViewSet(viewsets.ModelViewSet):
+    """
+    API View that provides CRUD functionality for the School entity.
+
+    URL:
+        /api/v1/schools/
+
+    Methods:
+        GET, PUT, POST, DELETE
+    """
+
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
     permissions = (AllowAny,)
 
 
 class StudentViewSet(viewsets.ModelViewSet):
+    """
+    API View that provides CRUD functionality for the Student entity.
+
+    URL:
+        /api/v1/students/
+
+    Methods:
+        GET, PUT, POST, DELETE
+    """
+
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permissions = (AllowAny,)    
+    permissions = (AllowAny,)

9e8c6c61a42f5a76933f7101f15b794119e74adb
Alex Kiura <alex.kiura@andela.com>
2017-07-20 12:40:36 +0100

    [Feature] Implement crud API for student entity

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import SchoolViewSet
+from .views import SchoolViewSet, StudentViewSet
 
 router = DefaultRouter()
+
 router.register('schools', SchoolViewSet)
+router.register('students', StudentViewSet)
 
 urlpatterns = router.urls
--- api_v1/views.py
+++ api_v1/views.py
@@ -4,10 +4,16 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
 from .serializers import SchoolSerializer, StudentSerializer
-from .models import School
+from .models import School, Student
 
 
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
     permissions = (AllowAny,)
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permissions = (AllowAny,)    

8be695600dd020ae34470eeca678a8c2c994e55a
Alex Kiura <alex.kiura@andela.com>
2017-07-20 11:34:15 +0100

    [Chore] Rename type field in school model

--- api_v1/tests/test_models.py
+++ api_v1/tests/test_models.py
@@ -1,4 +1,4 @@
-from .models import School, Student
+from api_v1.models import School
 from rest_framework.test import APITestCase
 
 
@@ -6,7 +6,7 @@ class SchoolTest(APITestCase):
     def test_school_creation(self):
         school = School.objects.create(
             name='Shule', email='shule@ymail.com', 
-            category='DISTRICT', type='BOARDING',
+            category='DISTRICT', school_type='BOARDING',
             gender='MIXED')
         self.assertEqual(1, School.objects.count())
         self.assertEqual(school.name, 'Shule')
